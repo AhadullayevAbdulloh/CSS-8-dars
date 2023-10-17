1003f964d31a0db6b9bf96d35ee96070976fb91e
AhadullayevAbdulloh <147840304+AhadullayevAbdulloh@users.noreply.github.com>
2023-10-17 20:14:22 +0500

    CSS 8-dars
    27-Guruh

---
+++ CSS 8-dars/js/main.js
@@ -0,0 +1,106 @@
+let byWrap = document.getElementsByTagName("div").namedItem("wrap");
+let byFromGroup = document.getElementsByTagName("div").namedItem("form-group");
+
+byWrap.style.width = "1200px";
+byWrap.style.margin = "0 auto";
+
+byFromGroup.style.width = "500px";
+byFromGroup.style.margin = "0 auto";
+byFromGroup.style.boxShadow = "0 0 10px rgba(80,80,80,0.5)";
+byFromGroup.style.padding = "1rem 1.2rem";
+byFromGroup.style.boxSizing ="border-box";
+
+let byfirstname = document.getElementsByTagName('input').namedItem('firstName')
+byfirstname.style.width = "60%"
+byfirstname.style.padding = "0.5rem 1.5rem"
+byfirstname.style.boxSizing = "border-box"
+byfirstname.style.border = "1px solid #cccccc"
+byfirstname.style.borderRadius = "4px"
+byfirstname.style.display = "inline-block"
+
+let bylastname = document.getElementsByTagName('input').namedItem('lastName')
+bylastname.style.width = "60%"
+bylastname.style.padding = "0.5rem 1.5rem"
+bylastname.style.boxSizing = "border-box"
+bylastname.style.border = "1px solid #cccccc"
+bylastname.style.borderRadius = "4px"
+bylastname.style.display = "inline-block"
+
+let bypreemail = document.getElementsByTagName('input').namedItem('preemail')
+bypreemail.style.width = "60%"
+bypreemail.style.padding = "0.5rem 1.5rem"
+bypreemail.style.boxSizing = "border-box"
+bypreemail.style.border = "1px solid #cccccc"
+bypreemail.style.borderRadius = "4px"
+bypreemail.style.display = "inline-block"
+
+let bypassword = document.getElementsByTagName('input').namedItem('password')
+bypassword.style.width = "60%"
+bypassword.style.padding = "0.5rem 1.5rem"
+bypassword.style.boxSizing = "border-box"
+bypassword.style.border = "1px solid #cccccc"
+bypassword.style.borderRadius = "4px"
+bypassword.style.display = "inline-block"
+
+let bylabel1 = document.getElementsByTagName('label').namedItem('label1')
+bylabel1.style.width = "35%"
+bylabel1.style.display = "inline-block"
+bylabel1.style.boxSizing = "border-box"
+bylabel1.style.padding = "0.5rem 1.5rem"
+bylabel1.style.marginBottom = "10px"
+bylabel1.style.textAlign = "right"
+
+let bylabel2 = document.getElementsByTagName('label').namedItem('label2')
+bylabel2.style.width = "35%"
+bylabel2.style.display = "inline-block"
+bylabel2.style.boxSizing = "border-box"
+bylabel2.style.padding = "0.5rem 1.5rem"
+bylabel2.style.marginBottom = "10px"
+bylabel2.style.textAlign = "right"
+
+let bylabel3 = document.getElementsByTagName('label').namedItem('label3')
+bylabel3.style.width = "35%"
+bylabel3.style.display = "inline-block"
+bylabel3.style.boxSizing = "border-box"
+bylabel3.style.padding = "0.5rem 1.5rem"
+bylabel3.style.marginBottom = "10px"
+bylabel3.style.textAlign = "right"
+
+let bylabel4 = document.getElementsByTagName('label').namedItem('label4')
+bylabel4.style.width = "35%"
+bylabel4.style.display = "inline-block"
+bylabel4.style.boxSizing = "border-box"
+bylabel4.style.padding = "0.5rem 1.5rem"
+bylabel4.style.marginBottom = "10px"
+bylabel4.style.textAlign = "right"
+
+let bylabel5 = document.getElementsByTagName('label').namedItem('label5')
+bylabel5.style.width = "35%"
+bylabel5.style.display = "inline-block"
+bylabel5.style.boxSizing = "border-box"
+bylabel5.style.padding = "0.5rem 1.5rem"
+bylabel5.style.marginBottom = "10px"
+bylabel5.style.textAlign = "right"
+
+let byselect = document.getElementsByTagName('select').namedItem('age')
+byselect.style.width = "20%"
+byselect.style.display = "inline-block"
+byselect.style.borderRadius = "4px"
+byselect.style.border = "1px solid #cccccc"
+byselect.style.padding = " 0.5rem 1.5rem"
+
+let bylabel6 = document.getElementsByTagName('label').namedItem('label6')
+bylabel6.style.width = "35%"
+bylabel6.style.display = "inline-block"
+bylabel6.style.boxSizing = "border-box"
+bylabel6.style.padding = "0.5rem 1.5rem"
+bylabel6.style.marginBottom = "10px"
+bylabel6.style.textAlign = "right"
+
+let byhappy = document.getElementsByTagName('select').namedItem('Brithday')
+byhappy.style.width = "20%"
+byhappy.style.display = "inline-block"
+byhappy.style.borderRadius = "4px"
+byhappy.style.border = "1px solid #cccccc"
+byhappy.style.padding = " 0.5rem 1.5rem"
+
